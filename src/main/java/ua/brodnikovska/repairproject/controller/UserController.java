bbf86e384c64eed603f68a1b1560f917813d8663
Brodnikovska <brodni01@kits.kfplc.com>
2021-11-11 16:30:30 +0200

    add mapping for userpage

---
+++ src/main/java/ua/brodnikovska/repairproject/controller/UserController.java
@@ -0,0 +1,26 @@
+package ua.brodnikovska.repairproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.brodnikovska.repairproject.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/userpage")
+    public String getUserPage() {
+        return "user/userpage";
+    }
+}
